7face10a13287b0c634174fa07393f76b312a67f
Hacker2999 <104303328+Hacker2999@users.noreply.github.com>
2023-11-15 13:36:57 +0300

    rework programs

--- solution/work1/main.cpp
+++ solution/work1/main.cpp
@@ -2,73 +2,87 @@
 #include <thread>
 #include <chrono>
 #include <string>
-#include "inih-master/ini.h"
-#include "inih-master/ini.c"
+
 #include "inih-master/cpp/INIReader.h"
-#include "inih-master/cpp/INIReader.cpp"
 
+class TrafficLight
+{
+    long int redt;
+    long int yellowt;
+    long int greent;
 
-
-class traffic_light{
-
-    int8_t redt;
-    int8_t yellowt;
-    int8_t greent;
+    void displayColor(const std::string &color)
+    {
+        if (color == "red")
+        {
+            std::cout << "\x1B[41m R \x1B[0m" << std::endl;
+        }
+        else if (color == "yellow")
+        {
+            std::cout << "\x1B[103m Y \x1B[0m" << std::endl;
+        }
+        else if (color == "green")
+        {
+            std::cout << "\x1B[42m G \x1B[0m" << std::flush;
+        }
+        else
+        {
+            std::cout << "Ошибка отображения цвета" << std::endl;
+            exit(1);
+        }
+    }
 
 public:
-
-    traffic_light()
+    TrafficLight()
     {
         INIReader reader("../timings.ini");
-        if (reader.ParseError() < 0){
-        redt=10;
-        yellowt=3;
-        greent=5;
+        if (reader.ParseError() < 0)
+        {
+            std::cout << "Ошибка прогрузки ini-файла" << std::endl;
+            exit(1);
         }
-        else{
-            redt= reader.GetInteger("timings","red",-1);
-            yellowt= reader.GetInteger("timings","yellow",-1);
-            greent=reader.GetInteger("timings","green",-1);
+        else
+        {
+            redt = reader.GetInteger("timings", "red", 5);
+            yellowt = reader.GetInteger("timings", "yellow", 3);
+            greent = reader.GetInteger("timings", "green", 5);
         }
     }
 
-    void displayColor(const std::string& color) {
-    if (color == "red") {
-        std::cout << "\x1B[41m   \x1B[0m\n";
-    }
-    else if (color == "yellow") {
-        std::cout << "\x1B[103m   \x1B[0m\n";
-    }
-    else if (color == "green") {
-        std::cout << "\x1B[42m   \x1B[0m\n";
-    }
-    }
-
-    void view() {
-
-        while (true) {
-        system("clear");
-        displayColor("red");
-        std::this_thread::sleep_for(std::chrono::seconds(redt));
-        system("clear");
-        displayColor("yellow");
-        std::this_thread::sleep_for(std::chrono::seconds(yellowt));
-        system("clear");
-        displayColor("green");
-        std::this_thread::sleep_for(std::chrono::seconds(greent));
-        for(int i= 0;i<4;i++)
-            {
+    void view()
+    {
+        while (true)
+        {
             system("clear");
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            displayColor("red");
+            std::this_thread::sleep_for(std::chrono::seconds(redt));
+            displayColor("yellow");
+            std::this_thread::sleep_for(std::chrono::seconds(yellowt));
+            system("clear");
+            std::cout << std::endl;
+            std::cout << std::endl;
             displayColor("green");
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            std::this_thread::sleep_for(std::chrono::seconds(greent));
+            for (int i = 0; i < 4; i++)
+            {
+                system("clear");
+                std::cout << std::endl;
+                std::cout << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                displayColor("green");
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
             }
+            system("clear");
+            std::cout << std::endl;
+            displayColor("yellow");
+            std::this_thread::sleep_for(std::chrono::seconds(yellowt));
         }
     }
-
 };
 
-int main() {
-    traffic_light aboba;
-    aboba.view();
+int main()
+{
+    TrafficLight trafficLight;
+    trafficLight.view();
+    return 0;
 }
