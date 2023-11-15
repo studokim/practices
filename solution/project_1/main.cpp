c48e0492dd00692e5413c82b549235be605fda26
TheIgor23 <egorchik210@gmail.com>
2023-11-15 14:35:04 +0300

    Algorithm fixed

--- solution/project_1/main.cpp
+++ solution/project_1/main.cpp
@@ -1,53 +1,67 @@
 #include <iostream>
-#include <vector>
 #include <thread>
 #include <chrono>
 #include "inih/cpp/INIReader.h"
-#include "inih/cpp/INIReader.cpp"
-#include "inih/ini.h"
-#include "inih/ini.c"
 using namespace std;
 
 
-struct Signal
+void displayColor(string& color, long timeSleepSecond)
 {
-	string color;
-	int time_s;
-};
+	cout<<color<<'\n';
+	this_thread::sleep_for(chrono::seconds(timeSleepSecond));		
+}
 
-class TrafficLights
+void displayAnimateColor(string& color, long timeSleepSecond)
+{
+	cout<<color<<'\n';
+	this_thread::sleep_for(chrono::seconds(timeSleepSecond));		
+	for (size_t i = 0; i < 3; i++)	
+	{
+		system("clear");
+		this_thread::sleep_for(chrono::milliseconds(600));	
+		cout<<color<<endl;
+		this_thread::sleep_for(chrono::milliseconds(600));	
+	}	
+}
+
+class TrafficLight
 {	
 private:
-
-	vector<Signal> signals{};
+	struct Signal
+	{
+		string color;
+		long time_s;
+		void (*displayColor)(string& color, long timeSleepSecond);
+	};
+	
+	const int SIGNAL_SIZE = 5;
+	Signal *signals;
 
 public:
-	TrafficLights(vector<Signal> s)
-	{
-		signals = s;
+	TrafficLight(long redTime, long greenTime, long yellowTime)
+	{		
+		signals = new Signal[SIGNAL_SIZE] 
+		{
+			{"\x1B[41m   \x1B[0m\n", redTime, displayColor},
+			{"\x1B[41m   \x1B[0m\n\x1B[43m   \x1B[0m\n", yellowTime, displayColor},
+			{"\n\n\x1B[42m   \x1B[0m\n", greenTime, displayAnimateColor},
+			{"\n\x1B[43m   \x1B[0m\n", yellowTime, displayColor},
+			{"\x1B[41m   \x1B[0m\n", redTime, displayColor}
+		};
 	}
 
 	void Start()
 	{
-		while (true)
-		{
-			for(int i = 0; i<signals.size(); i++)
-			{
-				
-				system("clear");
-				cout<<signals[i].color<<endl;
-				this_thread::sleep_for(chrono::seconds(signals[i].time_s));			
-			}
-			for (size_t i = 0; i < 3; i++)
-			{
-				system("clear");
-				this_thread::sleep_for(chrono::milliseconds(600));	
-				cout<<signals[2].color<<endl;
-				this_thread::sleep_for(chrono::milliseconds(600));	
-			}		
+		for(int i = 0; i<SIGNAL_SIZE; i++)
+		{			
+			system("clear");
+			signals[i].displayColor(signals[i].color, signals[i].time_s);	
+		}			
+	}
 
-		
-		}
+	~TrafficLight() 
+	{		
+		delete[] signals;
 	}
 };
 
@@ -56,24 +70,18 @@ int main()
 {
 	
 	INIReader reader("../timings.ini");
-	if (reader.ParseError() < 0) {
+	if (reader.ParseError() < 0) 
+	{
         std::cout << "Can't load 'timings.ini'\n";
         return 1;
     }
 
-	int redt = reader.GetInteger("timings", "red", 4), 
+	long redt = reader.GetInteger("timings", "red", 4), 
 	greent = reader.GetInteger("timings", "green", 1),
 	yellowt = reader.GetInteger("timings", "yellow", 4);
+	
 
-	vector<Signal> signals;
-	signals = 
-	{
-		{"\x1B[41m   \x1B[0m\n", redt},
-		{"\x1B[43m   \x1B[0m\n", greent},
-		{"\x1B[42m   \x1B[0m\n", yellowt},
-	};
-
-	TrafficLights trL(signals);
+	TrafficLight trL(redt, greent, yellowt);
 	trL.Start();
 	return 0;
 }
