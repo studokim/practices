38c331dcd8cd64965cf9554ade6be44eefec4c02
TheIgor23 <egorchik210@gmail.com>
2023-11-15 14:47:55 +0300

    remove build directory

--- solution/project_1/build/CMakeFiles/inih-objs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/inih-objs.dir/inih/cpp/INIReader.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/inih-objs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
