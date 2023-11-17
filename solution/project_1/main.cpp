26466e163e48b612318f7e9b411f8f5f785938c7
Rshfr <107305118+Rshfr@users.noreply.github.com>
2023-11-17 15:02:38 +0300

    Changed project

--- solution/project_1/main.cpp
+++ solution/project_1/main.cpp
@@ -3,12 +3,33 @@
 #include <thread>
 #include "./inih-master/cpp/INIReader.h"
 
-class TrafficLights
+class TrafficLight
 {
 private:
     int r_time, y_time, g_time;
+    
+    void cout_light(const std::string& color)
+    {
+        if(color == "red")
+        std::cout << "\033[41m   \033[0m" << std::endl;
+        
+        else if(color == "yellow")
+        std::cout << "\033[43m   \033[0m" << std::endl;
+        
+        else if(color == "green")
+        std::cout << "\033[42m   \033[0m\r" << std::flush;
+        
+        else if(color == "empty")
+        std::cout << "   \r" << std::flush;
+        
+        else
+        {
+            std::cout << "Unknown color: " << color << std::flush;
+            exit(-1);
+        }    
+    }
 public:
-    TrafficLights()
+    TrafficLight()
     {
         r_time = 5;
         y_time = 5;
@@ -16,34 +37,37 @@ public:
         
     }
     
-    TrafficLights(const std::string& path)
+    TrafficLight(const std::string& path)
     {
         INIReader reader(path);
         
-        if (reader.ParseError() < 0) {
-            std::cout << "Can't load 'settings.ini'\n";
+        if(reader.ParseError() == -2) 
+        {
+            std::cout << "Memory error\n";
             exit(-1);
         }
+        else if(reader.ParseError() == -1) 
+        {
+            std::cout << "Can't load 'settings.ini'\n";
+        }
+        else if(reader.ParseError() > 0)
+        {
+            std::cout << "'settings.ini' has a syntax error\nIn line: " << reader.ParseError() << std::endl;
+        }
         
-        r_time = reader.GetInteger("timings", "red", 2);
-        y_time = reader.GetInteger("timings", "yellow", 2);
-        g_time = reader.GetInteger("timings", "green", 2);
-    }
-    
-    void cout_light(const std::string& color)
-    {
-        if(color == "red")
-        std::cout << "\033[41m   \033[0m" << std::endl;
+        r_time = reader.GetInteger("timings", "red", -1);
+        y_time = reader.GetInteger("timings", "yellow", -1);
+        g_time = reader.GetInteger("timings", "green", -1);
         
-        if(color == "yellow")
-        std::cout << "\033[43m   \033[0m" << std::endl;
-        
-        if(color == "green")
-        std::cout << "\033[42m   \033[0m\r" << std::flush;
-        
-        if(color == "empty")
-        std::cout << "\033[45m   \033[0m\r" << std::flush;
+        if(r_time < 0 || y_time < 0 || g_time < 0)
+        {
+            std::cout << "Invalid timing value\nStandard values will be applied (2 seconds)\n";
+            system("sleep 4s");
             
+            r_time = 3;
+            y_time = 2;
+            g_time = 2;
+        }
     }
     
     void view_lights()
@@ -51,14 +75,15 @@ public:
         while (true)
         {   
             system("clear");
+            
             cout_light("red");
             std::this_thread::sleep_for(std::chrono::seconds(r_time));
-            
             
             cout_light("yellow");
             std::this_thread::sleep_for(std::chrono::seconds(y_time));
             
             system("clear");
+            std::cout << std::endl << std::endl;
             
             cout_light("green");
             std::this_thread::sleep_for(std::chrono::seconds(g_time));
@@ -72,22 +97,23 @@ public:
             }
             
             system("clear");
+            std::cout << std::endl;
             
             cout_light("yellow");
             std::this_thread::sleep_for(std::chrono::seconds(y_time));
             
-            
             std::cout << std::endl;
         }
     }
+    
+    
+    
 };
 
 int main()
 {
     
-    TrafficLights traffic_light("../settings.ini");
+    TrafficLight traffic_light("../settings.ini");
     
     traffic_light.view_lights();
-    
-    return 0;
 }
