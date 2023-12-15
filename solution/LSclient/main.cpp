643e00b31acd994681c8010b1984b0b2cecee6e5
Hacker2999 <104303328+Hacker2999@users.noreply.github.com>
2023-12-15 03:41:22 +0300

    Доработка

--- solution/LSclient/main.cpp
+++ solution/LSclient/main.cpp
@@ -1,22 +1,69 @@
 #include <iostream>
-#include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sstream>
+#include <regex>
 
-const int bufferSize = 1024;
+const int BufferSize = 1024;
+const int TimeoutSeconds = 5;
 
 class Client
 {
 private:
-    int serverSocket;
+    int clientSocket;
+
+    // Проверка статуса сервера
+    bool CheckServerStatus()
+    {
+        char statusBuffer[2];
+        ssize_t bytesRead = recv(clientSocket, statusBuffer, sizeof(statusBuffer) - 1, MSG_PEEK);
+
+        if (bytesRead == -1)
+        {
+            std::cerr << "Ошибка при проверке статуса сервера" << std::endl;
+            return false;
+        }
+        else if (bytesRead == 0)
+        {
+            return false; // Сервер отключен
+        }
+
+        return true; // Сервер работает
+    }
+
+    // Проверка задержки сервера
+    bool CheckServerDelay()
+    {
+        fd_set readSet;
+        FD_ZERO(&readSet);
+        FD_SET(clientSocket, &readSet);
+
+        struct timeval timeout;
+        timeout.tv_sec = TimeoutSeconds;
+        timeout.tv_usec = 0;
+
+        int result = select(clientSocket + 1, &readSet, nullptr, nullptr, &timeout);
+
+        if (result == -1)
+        {
+            std::cerr << "Ошибка при проверке задержки сервера" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        else if (result == 0)
+        {
+            std::cerr << "Задержка со стороны сервера. Пожалуйста, подождите." << std::endl;
+            return true;
+        }
+
+        return false;
+    }
 
 public:
-    Client(const std::string &serverIP, int serverPort)
+
+    Client(int clientPort, const std::string &serverIP, int serverPort)
     {
         // Создание сокета клиента
-        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-        if (serverSocket == -1)
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == -1)
         {
             std::cerr << "Ошибка создания сокета" << std::endl;
             exit(EXIT_FAILURE);
@@ -30,112 +77,157 @@ public:
         if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0)
         {
             std::cerr << "Некорректный адрес" << std::endl;
-            close(serverSocket);
+            close(clientSocket);
             exit(EXIT_FAILURE);
         }
 
-        // Установка соединения с сервером
-        std::cout << "Подключение к серверу " << serverIP << ":" << serverPort << std::endl;
-        if (connect(serverSocket, reinterpret_cast<sockaddr *>(&serverAddr), sizeof(serverAddr)) == -1)
+        // Установка параметров клиента
+        sockaddr_in clientAddr{};
+        memset(&clientAddr, 0, sizeof(clientAddr));
+        clientAddr.sin_family = AF_INET;
+        clientAddr.sin_port = htons(clientPort);
+        clientAddr.sin_addr.s_addr = INADDR_ANY;
+
+        // Привязка сокета к порту клиента
+        if (bind(clientSocket, reinterpret_cast<sockaddr *>(&clientAddr), sizeof(clientAddr)) == -1)
+        {
+            std::cerr << "Ошибка привязки сокета к порту" << std::endl;
+            close(clientSocket);
+            exit(EXIT_FAILURE);
+        }
+
+        // Подключение к серверу
+        std::cout << "Подключение к серверу " << serverIP << ":" << serverPort << " с портом клиента " << clientPort << std::endl;
+        if (connect(clientSocket, reinterpret_cast<sockaddr *>(&serverAddr), sizeof(serverAddr)) == -1)
         {
             std::cerr << "Ошибка подключения к серверу" << std::endl;
-            close(serverSocket);
+            close(clientSocket);
             exit(EXIT_FAILURE);
         }
 
-        std::cout << "Подключен к серверу " << serverIP << ":" << serverPort << std::endl;
+        std::cout << "Подключен к серверу " << serverIP << ":" << serverPort << " с портом клиента " << clientPort << std::endl;
     }
 
-    // Запуск клиента
-    void start()
+    // Проверка валидности команды
+    bool IsValidCommand(const std::string &command)
     {
-        char buffer[bufferSize];
-
-        while (true)
-        {
-
-            std::cout << "> ";
-            std::string command;
-            std::getline(std::cin, command);
-
-            if (command.empty())
-            {
-                std::cout << "Неверная команда. Введите команду." << std::endl;
-                continue;
-            }
-
-            sendCommand(command);
-
-            receiveResponse(buffer, sizeof(buffer));
-            std::cout << buffer << std::endl;
-        }
+        std::regex lsCommandPattern("^ls(\\s*-?[ahl]+\\s*)*(\\s+/[a-zA-Z0-9_/]+)?$");
+        return std::regex_match(command, lsCommandPattern);
     }
 
     // Отправка команды на сервер
-    void sendCommand(const std::string &command)
+    void SendCommand(const std::string &command)
     {
         // Проверка успешной отправки команды
-        if (send(serverSocket, command.c_str(), command.size(), 0) == -1)
+        ssize_t sentBytes = send(clientSocket, command.c_str(), command.size(), 0);
+        if (sentBytes == -1)
         {
             std::cerr << "Ошибка отправки на сервер" << std::endl;
-            close(serverSocket);
+            close(clientSocket);
+            exit(EXIT_FAILURE);
+        }
+        else if (sentBytes != static_cast<ssize_t>(command.size()))
+        {
+            std::cerr << "Не все данные были отправлены" << std::endl;
+            close(clientSocket);
             exit(EXIT_FAILURE);
         }
     }
 
     // Получение ответа от сервера
-    void receiveResponse(char *buffer, int bufferSize)
+    void ReceiveResponse(char *buffer, int bufferSize)
     {
-        // Получение ответа от сервера
-        ssize_t bytesRead = recv(serverSocket, buffer, bufferSize - 1, 0);
+        ssize_t bytesRead = recv(clientSocket, buffer, bufferSize - 1, 0);
         if (bytesRead == -1)
         {
             std::cerr << "Ошибка получения ответа от сервера" << std::endl;
-            close(serverSocket);
+            close(clientSocket);
             exit(EXIT_FAILURE);
         }
         else if (bytesRead == 0)
         {
             // Обработка отключения сервера
             std::cerr << "Сервер отключен" << std::endl;
-            close(serverSocket);
+            std::cout << "Нажмите Enter для завершения программы." << std::endl;
+            std::cin.get();
             exit(EXIT_FAILURE);
         }
 
         buffer[bytesRead] = '\0';
     }
 
+    // Запуск клиента
+    void Start()
+    {
+        char buffer[BufferSize];
+
+        while (true)
+        {
+            std::cout << "> ";
+            std::string command;
+            std::getline(std::cin, command);
+
+            // Проверка пустой команды или неверной команды
+            if (command.empty() || !IsValidCommand(command))
+            {
+                std::cout << "Неверная команда. Введите корректную команду." << std::endl;
+                continue;
+            }
+
+            SendCommand(command);
+
+            // Проверка задержки сервера
+            CheckServerDelay();
+
+            // Проверка статуса сервера
+            if (!CheckServerStatus())
+            {
+                std::cerr << "Сервер отключен" << std::endl;
+                std::cout << "Нажмите Enter для завершения программы." << std::endl;
+                std::cin.get();
+                exit(EXIT_FAILURE);
+            }
+
+            // Получение и вывод ответа от сервера
+            ReceiveResponse(buffer, sizeof(buffer));
+            std::cout << buffer << std::endl;
+        }
+    }
+
+    // Деструктор класса Client
     ~Client()
     {
-        close(serverSocket);
+        shutdown(clientSocket, SHUT_RDWR);
+        close(clientSocket);
     }
 };
 
+
 int main(int argc, char *argv[])
 {
-    // Проверка аргументов командной строки
+    // Проверка количества аргументов командной строки
     if (argc != 3)
     {
-        std::cerr << "Используется: " << argv[0] << " <serverPort> <serverAddress:proxyPort>" << std::endl;
+        std::cerr << "Используется: " << argv[0] << " <clientPort> <serverAddress:serverPort>" << std::endl;
         return 1;
     }
 
-    const int serverPort = std::stoi(argv[1]);
+    const int clientPort = std::stoi(argv[1]);
 
     std::istringstream serverInfo(argv[2]);
     std::string serverAddress, serverPortStr;
 
-    // Проверка успешного чтения адреса прокси и порта
+    // Проверка успешного извлечения адреса и порта сервера
     if (!std::getline(serverInfo, serverAddress, ':') || !std::getline(serverInfo, serverPortStr))
     {
         std::cerr << "Неверный адрес или порт" << std::endl;
         return 1;
     }
 
-    const int proxyPort = std::stoi(serverPortStr);
+    const int serverPort = std::stoi(serverPortStr);
 
-    Client client(serverAddress, proxyPort);
-    client.start();
+    Client client(clientPort, serverAddress, serverPort);
+    client.Start();
 
     return 0;
 }
